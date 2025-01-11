a920700cd60d7176e591002f43535fa657315b90
Xougui <xougui.7@gmail.com>
2025-01-11 19:42:59 +0100

    changements2

---
+++ cog_exemple.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+class Exemple(commands.Cog):
+	
+    def __init__(self, bot):
+        self.bot = bot
+            
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Cog loaded : cog exemple")
+
+    # -------------------------------------------------------------------------
+    # -----------------------code ici------------------------------------------
+    @bot.tree.command(name="exemple", description="exemple")
+    async def ping(self,interaction: discord.Interaction):
+        await interaction.response.send_message("Exemple")
+    # -------------------------------------------------------------------------
+
+@bot.event
+async def setup_hook() -> None:
+    synced = await bot.tree.sync() #sync ici
+    print(f"Synced {len(synced)} commands")
+
+async def setup(bot):
+    await bot.add_cog(Exemple(bot))
