4b13371a76848c6dcc55c326488dda8b34c75cad
Xougui <xougui.7@gmail.com>
2025-09-27 12:56:45 +0200

    Update Layout_exemple.py

--- cog/Layout_exemple.py
+++ cog/Layout_exemple.py
@@ -38,7 +38,7 @@ class Reactionsv2(LayoutView):
 
 
 class Reactions(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
  
     @app_commands.command(name="Test-layout", description="Embed v2")
